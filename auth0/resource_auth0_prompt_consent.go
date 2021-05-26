ec4fa6d4f7b8c652d0e67e58ca3437a63f8cd274
Art Huston <art@abacusinsights.com>
2021-05-26 11:34:50 -0400

    Add language support, unpack and return consent

--- auth0/resource_auth0_prompt_consent.go
+++ auth0/resource_auth0_prompt_consent.go
@@ -1,139 +1,164 @@
 package auth0
-//
-//import (
-//	"net/http"
-//
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-//	"gopkg.in/auth0.v5/management"
-//)
-//
-//func newPromptConsent() *schema.Resource {
-//
-//	return &schema.Resource{
-//
-//		Create: createPromptConsent,
-//		Read:   readPromptConsent,
-//		Update: updatePromptConsent,
-//		Delete: deletePromptConsent,
-//		Importer: &schema.ResourceImporter{
-//			State: schema.ImportStatePassthrough,
-//		},
-//		Schema: map[string]*schema.Schema{
-//			"consent": {
-//				Type:     schema.TypeList,
-//				Optional: true,
-//				Computed: true,
-//				MaxItems: 1,
-//				MinItems: 1,
-//				Elem: &schema.Resource{
-//					Schema: map[string]*schema.Schema{
-//						"page_title": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"title": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"picker_title": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"message_multiple_tenants": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"audience_picker_alt_text": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"message_single_tenant": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"accept_button_text": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"decline_button_text": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"invalid_action": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"invalid_audience": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//						"invalid_scope": {
-//							Type:     schema.TypeString,
-//							Optional: true,
-//						},
-//					},
-//				},
-//			},
-//		},
-//	}
-//}
-//
-//func createPromptConsent(d *schema.ResourceData, m interface{}) error {
-//	d.SetId(resource.UniqueId())
-//	return updatePromptConsent(d, m)
-//}
-//
-//func readPromptConsent(d *schema.ResourceData, m interface{}) error {
-//	api := m.(*management.Management)
-//	p, err := api.Prompt.Read()
-//	if err != nil {
-//		if mErr, ok := err.(management.Error); ok {
-//			if mErr.Status() == http.StatusNotFound {
-//				d.SetId("")
-//				return nil
-//			}
-//		}
-//		return err
-//	}
-//	d.Set("universal_login_experience", p.UniversalLoginExperience)
-//	d.Set("identifier_first", p.IdentifierFirst)
-//	return nil
-//}
-//
-//func updatePromptConsent(d *schema.ResourceData, m interface{}) error {
-//	p := buildPromptConsent(d)
-//	api := m.(*management.Management)
-//	err := api.Prompt.ReplacePromptConsent(p)
-//	if err != nil {
-//		return err
-//	}
-//	return readPromptConsent(d, m)
-//}
-//
-//func deletePromptConsent(d *schema.ResourceData, m interface{}) error {
-//	d.SetId("")
-//	return nil
-//}
-//
-//func buildPromptConsent(d *schema.ResourceData) *management.PromptConsent {
-//	p := &management.PromptConsent{}
-//
-//	List(d, "consent").Elem(func(d ResourceData) {
-//		p.Consent = &management.ScreenConsent{
-//			PageTitle:              *String(d, "page_title"),
-//			Title:                  *String(d, "title"),
-//			PickerTitle:            *String(d, "picker_title"),
-//			MessageMultipleTenants: *String(d, "message_multiple_tenants"),
-//			AudiencePickerAltText:  *String(d, "audience_picker_alt_text"),
-//			MessageSingleTenant:    *String(d, "message_single_tenant"),
-//			AcceptButtonText:       *String(d, "accept_button_text"),
-//			DeclineButtonText:      *String(d, "decline_button_text"),
-//			InvalidAction:          *String(d, "invalid_action"),
-//			InvalidAudience:        *String(d, "invalid_audience"),
-//			InvalidScope:           *String(d, "invalid_scope"),
-//		}
-//	})
-//
-//	return p
-//}
+
+import (
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"gopkg.in/auth0.v5/management"
+)
+
+func newPromptConsent() *schema.Resource {
+
+	return &schema.Resource{
+
+		Create: createPromptConsent,
+		Read:   readPromptConsent,
+		Update: updatePromptConsent,
+		Delete: deletePromptConsent,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"language": {
+				Type: schema.TypeString,
+				Optional: false,
+			},
+			"consent": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				MaxItems: 1,
+				MinItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"page_title": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"title": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"picker_title": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"message_multiple_tenants": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"audience_picker_alt_text": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"message_single_tenant": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"accept_button_text": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"decline_button_text": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"invalid_action": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"invalid_audience": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"invalid_scope": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func language(d *schema.ResourceData) string {
+	return *String(d, "language")
+}
+
+func createPromptConsent(d *schema.ResourceData, m interface{}) error {
+	d.SetId(resource.UniqueId())
+	return updatePromptConsent(d, m)
+}
+
+func flattenScreenConsent(pc *management.ScreenConsent) []interface{} {
+	m := make(map[string]interface{})
+	if pc != nil {
+		m["page_title"] = pc.PageTitle
+		m["title"] = pc.Title
+		m["picker_title"] = pc.PickerTitle
+		m["message_multiple_tenants"] = pc.MessageMultipleTenants
+		m["audience_picker_alt_text"] = pc.AudiencePickerAltText
+		m["message_single_tenant"] = pc.MessageSingleTenant
+		m["accept_button_text"] = pc.AcceptButtonText
+		m["decline_button_text"] = pc.DeclineButtonText
+		m["invalid_action"] = pc.InvalidAction
+		m["invalid_audience"] = pc.InvalidAudience
+		m["invalid_scope"] = pc.InvalidScope
+	}
+	return []interface{}{m}
+}
+
+func readPromptConsent(d *schema.ResourceData, m interface{}) error {
+	api := m.(*management.Management)
+	pc, err := api.Prompt.ReadPromptConsent(language(d))
+	if err != nil {
+		if mErr, ok := err.(management.Error); ok {
+			if mErr.Status() == http.StatusNotFound {
+				d.SetId("")
+				return nil
+			}
+		}
+		return err
+	}
+	d.Set("consent", flattenScreenConsent(pc.Consent))
+	return nil
+}
+
+func updatePromptConsent(d *schema.ResourceData, m interface{}) error {
+	pc := buildPromptConsent(d)
+	api := m.(*management.Management)
+	err := api.Prompt.ReplacePromptConsent(language(d), pc)
+	if err != nil {
+		return err
+	}
+	return readPromptConsent(d, m)
+}
+
+func deletePromptConsent(d *schema.ResourceData, m interface{}) error {
+	d.SetId("")
+	return nil
+}
+
+func buildPromptConsent(d *schema.ResourceData) *management.PromptConsent {
+	pc := &management.PromptConsent{}
+
+	List(d, "consent").Elem(func(d ResourceData) {
+		pc.Consent = &management.ScreenConsent{
+			PageTitle:              *String(d, "page_title"),
+			Title:                  *String(d, "title"),
+			PickerTitle:            *String(d, "picker_title"),
+			MessageMultipleTenants: *String(d, "message_multiple_tenants"),
+			AudiencePickerAltText:  *String(d, "audience_picker_alt_text"),
+			MessageSingleTenant:    *String(d, "message_single_tenant"),
+			AcceptButtonText:       *String(d, "accept_button_text"),
+			DeclineButtonText:      *String(d, "decline_button_text"),
+			InvalidAction:          *String(d, "invalid_action"),
+			InvalidAudience:        *String(d, "invalid_audience"),
+			InvalidScope:           *String(d, "invalid_scope"),
+		}
+	})
+
+	return pc
+}
--- auth0/resource_auth0_prompt_consent_test.go
+++ auth0/resource_auth0_prompt_consent_test.go
@@ -1,89 +1,91 @@
 package auth0
-//
-//import (
-//	"testing"
-//
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-//	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-//)
-//
-//func TestAccPromptConsent(t *testing.T) {
-//
-//	resource.Test(t, resource.TestCase{
-//		Providers: map[string]terraform.ResourceProvider{
-//			"auth0": Provider(),
-//		},
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testAccPromptConsentCreate,
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.page_title", "page_title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.title", "title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.picker_title", "picker_title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_multiple_tenants", "message_multiple_tenants"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.audience_picker_alt_text", "audience_picker_alt_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_single_tenant", "message_single_tenant"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.accept_button_text", "accept_button_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.decline_button_text", "decline_button_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_action", "invalid_action"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_audience", "invalid_audience"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_scope", "invalid_scope"),
-//				),
-//			},
-//			{
-//				Config: testAccPromptConsentUpdate,
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.page_title", "updated_page_title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.title", "updated_title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.picker_title", "updated_picker_title"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_multiple_tenants", "updated_message_multiple_tenants"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.audience_picker_alt_text", "updated_audience_picker_alt_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_single_tenant", "updated_message_single_tenant"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.accept_button_text", "updated_accept_button_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.decline_button_text", "updated_decline_button_text"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_action", "updated_invalid_action"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_audience", "updated_invalid_audience"),
-//					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_scope", "updated_invalid_scope"),
-//				),
-//			},
-//		},
-//	})
-//}
-//
-//const testAccPromptConsentCreate = `
-//
-//resource "auth0_prompt_consent" "prompt_consent" {
-// consent {
-//   page_title = "page_title"
-//   title = "title"
-//   picker_title = "picker_title"
-//   message_multiple_tenants = "message_multiple_tenants"
-//   audience_picker_alt_text = "audience_picker_alt_text"
-//   message_single_tenant = "message_single_tenant"
-//   accept_button_text = "accept_button_text"
-//   decline_button_text = "decline_button_text"
-//   invalid_action = "invalid_action"
-//   invalid_audience = "invalid_audience"
-//   invalid_scope = "invalid_scope"
-// }
-//}
-//`
-//
-//const testAccPromptConsentUpdate = `
-//
-//resource "auth0_prompt_consent" "prompt_consent" {
-//	consent {
-//		page_title = "updated_page_title"
-//		title = "updated_title"
-//		picker_title = "updated_picker_title"
-//		message_multiple_tenants = "updated_message_multiple_tenants"
-//		audience_picker_alt_text = "updated_audience_picker_alt_text"
-//		message_single_tenant = "updated_message_single_tenant"
-//		accept_button_text = "updated_accept_button_text"
-//		decline_button_text = "updated_decline_button_text"
-//		invalid_action = "updated_invalid_action"
-//		invalid_audience = "updated_invalid_audience"
-//		invalid_scope = "updated_invalid_scope"
-//	}
-//}
-//`
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccPromptConsent(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]terraform.ResourceProvider{
+			"auth0": Provider(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPromptConsentCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.page_title", "page_title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.title", "title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.picker_title", "picker_title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_multiple_tenants", "message_multiple_tenants"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.audience_picker_alt_text", "audience_picker_alt_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_single_tenant", "message_single_tenant"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.accept_button_text", "accept_button_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.decline_button_text", "decline_button_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_action", "invalid_action"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_audience", "invalid_audience"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_scope", "invalid_scope"),
+				),
+			},
+			{
+				Config: testAccPromptConsentUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.page_title", "updated_page_title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.title", "updated_title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.picker_title", "updated_picker_title"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_multiple_tenants", "updated_message_multiple_tenants"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.audience_picker_alt_text", "updated_audience_picker_alt_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.message_single_tenant", "updated_message_single_tenant"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.accept_button_text", "updated_accept_button_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.decline_button_text", "updated_decline_button_text"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_action", "updated_invalid_action"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_audience", "updated_invalid_audience"),
+					resource.TestCheckResourceAttr("auth0_prompt_consent.prompt_consent", "consent.0.invalid_scope", "updated_invalid_scope"),
+				),
+			},
+		},
+	})
+}
+
+const testAccPromptConsentCreate = `
+
+resource "auth0_prompt_consent" "prompt_consent" {
+	consent {
+		language = "en"
+		  page_title = "page_title"
+		  title = "title"
+		  picker_title = "picker_title"
+		  message_multiple_tenants = "message_multiple_tenants"
+		  audience_picker_alt_text = "audience_picker_alt_text"
+		  message_single_tenant = "message_single_tenant"
+		  accept_button_text = "accept_button_text"
+		  decline_button_text = "decline_button_text"
+		  invalid_action = "invalid_action"
+		  invalid_audience = "invalid_audience"
+		  invalid_scope = "invalid_scope"
+		}
+	}
+`
+
+const testAccPromptConsentUpdate = `
+
+resource "auth0_prompt_consent" "prompt_consent" {
+	consent {
+		language = "en"
+		page_title = "updated_page_title"
+		title = "updated_title"
+		picker_title = "updated_picker_title"
+		message_multiple_tenants = "updated_message_multiple_tenants"
+		audience_picker_alt_text = "updated_audience_picker_alt_text"
+		message_single_tenant = "updated_message_single_tenant"
+		accept_button_text = "updated_accept_button_text"
+		decline_button_text = "updated_decline_button_text"
+		invalid_action = "updated_invalid_action"
+		invalid_audience = "updated_invalid_audience"
+		invalid_scope = "updated_invalid_scope"
+	}
+}
+`
